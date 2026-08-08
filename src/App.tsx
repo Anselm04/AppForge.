78270f05e94884789f66df6668562886acad1b31
Anselm04 <anselm.perkins@gmail.com>
2026-08-08 15:59:41 +1200

    feat: add landing account controls theme language and pricing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home';
 import Build from './pages/Build';
+import LandingEnhancements from './components/LandingEnhancements';
 
 export default function App() {
-  return window.location.pathname === '/build' ? <Build /> : <Home />;
+  return window.location.pathname === '/build' ? <Build /> : <LandingEnhancements><Home /></LandingEnhancements>;
 }
