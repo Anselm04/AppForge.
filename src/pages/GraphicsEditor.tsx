9d6dbca7aed17e31857a9c5efe65f718d33e55ac
Anselm04 <anselm.perkins@gmail.com>
2026-08-28 23:03:18 +1200

    test path ref

--- src/pages/GraphicsEditor.tsx
+++ src/pages/GraphicsEditor.tsx
@@ -1,8 +1,1 @@
-import { useState, useRef, useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import { useQuery, useMutation } from "@tanstack/react-query";
-import { trpc } from "../utils/trpc.js";
-
-export function GraphicsEditor() {
-  return <div>Graphics Editor parity pending full upload</div>;
-}
+TEST_PATH_REF
